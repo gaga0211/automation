75dd5395a6f721a558765430379341c94aedb33e
嘎嘎 <gaga@gagadeMBP.lan>
2018-11-04 11:09:04 +0800

    add

--- python01.py
+++ python01.py
@@ -17,6 +17,15 @@ for i in range(1,101):
 #如果字符串长度大于 3，添加 ‘ing’ 到字符串的末尾
 # 如果字符串是以 ‘ing’ 结尾，就在末尾添加 ‘ly’
 # 如果字符串长度小于 3，返回原字符串
+str = input("请输入字符串：")
+if len(str) >= 3:
+    if str.endswith('ing'):
+        str += 'ly'
+    else:
+        str += 'ing'
+else:
+    pass
+print(str)
 
 
 # 判断回文 回文：62426 为回文数字
